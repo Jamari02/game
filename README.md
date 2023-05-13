dab43190cfdc25781729bb832e8971254120f47f
Jamari02 <castrojamari@gmail.com>
2023-05-13 19:12:09 -0400

    Completed Projection

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
+
 # game
+# Project Proposal Example
+
+## Project Choice (Tell us which project you're doing!)
+
+- [ ] Trivia (Self-scoring)
+
+## Project Description 
+
+My app is called AniTrivia. It's a game app for people who want to test their anime and manga knowledge. The player will answer 10 questions with random facts and points will be tallied. The score will be displayed once the player has answered all the questions. 
+
+## Wire Frames
+
+**Initial Landing View**
+
+![image](https://i.imgur.com/eY2TAih.jpg)
+
+**Results View**
+
+![image](https://i.imgur.com/tODFGFz.jpg)
+
+## User Stories
+
+- As a player, I want my game to recognize when a question is answered correctly 
+- As a player, I would like to be informed of the score once completed 
+- As a player, I would like to have four different answer choices 
+- As a player I would like to be able to restart the game after completing the questions
+
+#### MVP Goals
+
+- I want a function to compare to chosen answer to the correct answer and return correct or incorrect
+-I want a function that keeps track of the correct answers and displays the amount of correct answers compared to (total) amount of questions 
+-I would like to display four seperate answers in accordance with the current question by using an array to call an answer choice into each innerText
+-I want a restart game button the will reset all the questions and score counter once clicked 
+
+#### Stretch Goals
+
+- As a player, I would like a animation for if I score 100 
+- As a player, I would like to hear an audible sound when I pick my answer 
+- As a player, I would like to have different categories that cater to beginner to expert knowledge 
--- script.js
+++ script.js
@@ -1,144 +1,330 @@
-
-// DOM elements
+// Dom Elememnts
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
-const questionContainerEl = document.getElementById('question-container')
-let randomQuestions, currentQuestionIndex
+const submitButton = document.getElementById('submit')
+const restartButton = document.getElementById('restart')
+const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
+const scoreText = document.getElementById("score");
 
-let correctAnswer = []
-const MAX_QUESTIONS = 10
+//Randomize Questions
+let shuffledQuestions, currentQuestionIndex
+let score = 0;
 
-const highScore = document.getElementById('highScore')
+restartButton.addEventListener('click', restartGame)
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+submitButton.addEventListener('click', showScore)
 
-// Variables
-let questions = [
-  {
-    questions: 'In Jujutsu Kaisen what is the name for Gojos eyes?',
-    answers: ['All seeing', 'God eyes', 'Six eyes', 'Eyes of destiny'],
-    answer: 'Six eyes'
-  },
-  {
-    questions: 'Who killed Noelles mother in black clover?',
-    answers: ['Vanica', 'Dante', 'Zenon', 'She died from a disease'],
-    answer: 'Vanica'
-  },
-  {
-    questions: 'Where did Tanjiro learn the Hinokami Kaguara from?',
-    answers: [
-      'He acted on pure instinct',
-      'His Father',
-      'A lonely old man',
-      'Scrolls he found while training'
-    ],
-    answer: 'His Father'
-  },
-  {
-    questions: 'How did Pochita become Denjis heart in Chainsaw Man? ',
-    answers: [
-      'Denji ate Pochita',
-      'A contract after Denji died',
-      'Open heart surgery to save his life',
-      'Another devil forced them together'
-    ],
-    answer: 'A contract after Denji died'
-  },
-  {
-    questions: 'How many users of one for all have existed?',
-    answers: ['4', '7', '9', '8'],
-    answer: '9'
-  },
-  {
-    questions: 'Who is the number 1 Hero in One Punch Man ',
-    answers: ['Saitama', 'Tatsumaki', 'Blast', 'Flashy Flash'],
-    answer: 'Blast'
-  },
-  {
-    questions: 'Who’s Mob’s mentor in Mob Psycho 100?',
-    answers: ['Reigen', 'Dimple', 'Ritsu', 'Teruki'],
-    answer: 'Reigen'
+let executed = false;
+function startGame() {
+  if (!executed) {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() * questions)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+    executed = true;
+
   }
 
-]
-
-// Start game function, once you click on the start game button this function will execute
-function startGame() {
-console.log('Started')
-startButton.classList.add('hide')
-randomQuestions = questions.sort(() => Math.random() - .5)
-currentQuestionIndex = 0
-questionContainerEl.classList.remove('hide')
-nextQuestion()
 }
 
-function nextQuestion() {
-    resetState()
-   showQuestion(randomQuestions[currentQuestionIndex])
-
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-    questionElement.innerText = question.questions
-    question.answers.forEach(answer => {
-        const button =document.createElement('button')
-        button.innerText = answer
-        button.classList.add('btn')
-        if(answer === question.answer) {
-            button.dataset.correct = true
-            correctAnswer.push(answer)
-            console.log(correctAnswer)
-            highScore.innerText = correctAnswer.length
-        }
-        button.addEventListener('click', selectAnswer)
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', function(e){
+        selectAnswer(e, answer);
+    }, false);
+
+    if (currentQuestionIndex  < shuffledQuestions.length + 1) {
         answerButtonsElement.appendChild(button)
+    }
     
-    })
+  })
 }
 
 function resetState() {
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
 }
 
-function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-      setStatusClass(button, button.dataset.correct)
-    }) 
-    if (randomQuestions.length > currentQuestionIndex + 1) {
-      nextButton.classList.remove('hide')
-    } else {
-      startButton.innerText = 'Restart'
-      startButton.classList.remove('hide')
-    }
+function selectAnswer(e, answer) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (answer.correct) {
+    score++;
+  }
+  
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    submitButton.classList.remove('hide')
+    restartButton.classList.remove('hide')
   }
 
-  
+}
 
-function setStatusClass(element, correctAnswer) {
-    clearStatusClass(element)
-    if(correctAnswer) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
+function showScore () {
+    document.getElementById("score").innerHTML = "you got " + score + " correct.";
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
 }
 
 function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
+function restartGame() {
+    // Reset the current question index and score
+    currentQuestionIndex = 0;
+    score = 0;
+  
+    // Clear the answer buttons
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    }
+  
+    // Hide the submit and restart buttons
+    submitButton.classList.add('hide');
+    restartButton.classList.add('hide');
+  
+    // Show the question container and start the game
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
+  }
+  
 
-//Event Listeners
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-    currentQuestionIndex++
-    nextQuestion()
-})
+const questions = [
+    {
 
+    question: 'Who killed Noelles mother in black clover?',
+    answers: [{
+        text: 'Dante',
+        correct: false
+    },
+     {
+        text: 'Vanica',
+        correct: true
+      },
+      {
+        text: 'Zenon',
+        correct: false
+      },
+      {
+        text: 'She died from a disease',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'In Jujutsu Kaisen what is the name for Gojos eyes?',
+    answers: [{
+        text: 'Six eyes',
+        correct: true
+      },
+      {
+        text: 'All Seeing',
+        correct: false
+      },
+      {
+        text: 'Eyes of destiny',
+        correct: false
+      },
+      {
+        text: 'God eyes',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'Where did Tanjiro learn the Hinokami Kaguara from?',
+    answers: [{
+        text: 'He acted on pure instinct',
+        correct: false
+      },
+      {
+        text:  'A lonely old man',
+        correct: false
+      },
+      {
+        text: 'His Father',
+        correct: true
+      },
+      {
+        text: 'Scrolls he found while training',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'How did Pochita become Denjis heart in Chainsaw Man? ',
+    answers: [{
+        text: 'Denji ate Pochita',
+        correct: false
+      },
+      {
+        text: 'Open heart surgery to save his life',
+        correct: false
+      },
+      {
+        text: 'Another devil forced them together',
+        correct: false
+      },
+      {
+        text: 'A contract after Denji died',
+        correct: true
+      }
+    ]
+  },
+  {
+    question: 'How many users of one for all have existed?',
+    answers: [{
+        text: '9',
+        correct: true
+      },
+      {
+        text: '7',
+        correct: false
+      },
+      {
+        text: '8',
+        correct: false
+      },
+      {
+        text: '4',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'Who is the number 1 Hero in One Punch Man ',
+    answers: [{
+        text: 'Saitama',
+        correct: false
+      },
+      {
+        text: 'Tatsumaki',
+        correct: false
+      },
+      {
+        text: 'Blast',
+        correct: true
+      },
+      {
+        text: 'Flashy Flash',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'Who’s Mob’s mentor in Mob Psycho 100?',
+    answers: [{
+        text: 'Reigen',
+        correct: true
+      },
+      {
+        text: 'Dimple',
+        correct: false
+      },
+      {
+        text: 'Ritsu',
+        correct: false
+      },
+      {
+        text: 'Teruki',
+        correct: false
+      }
+    ]
+  },
+  {
+    question:  'How long was Senku petrified?',
+    answers: [{
+        text: ' 5000 years ',
+        correct: false
+      },
+      {
+        text: ' 2585 years ',
+        correct: false
+      },
+      {
+        text: '3700 years',
+        correct: true
+      },
+      {
+        text: '800 years',
+        correct: false
+      }
+    ]
+  },
+  {
+    question:  ' In The Promised Neverland what is the relationship between Ray and Isabel?',
+    answers: [{
+        text: 'Foster family',
+        correct: false
+      },
+      {
+        text: 'Sister/ Brother',
+        correct: false
+      },
+      {
+        text: 'Mother/ Son',
+        correct: true
+      },
+      {
+        text: 'Aunt/ Nephew',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'Who is considered the strongest character in the story of Vinland Saga?',
+    answers: [{
+        text: ' Thorkell',
+        correct: false
+      },
+      {
+        text: ' Floki ',
+        correct: false
+      },
+      {
+        text: 'Askeladd',
+        correct: false
+      },
+      {
+        text: 'Thors ',
+        correct: true
+      }
+    ]
+  }
+]
